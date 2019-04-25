df3d47c78948107d9aed0e63e89387ae2f2b2d51
Darrion Gering and Liz Kelley <liz@>
2019-04-25 16:33:00 -0700

    add more dictionaries, number string test, array returns string test

--- Numbers.Tests/ModelTests/NumberTests.cs
+++ Numbers.Tests/ModelTests/NumberTests.cs
@@ -15,5 +15,23 @@ namespace NumbersToWords.Tests
       Numbers testNumber = new Numbers("5");
       Assert.AreEqual(typeof(Numbers), testNumber.GetType());
     }
+
+    [TestMethod]
+    public void MakeArray_NumberString_IntArray()
+    {
+      Numbers testNumber = new Numbers("532");
+      char[] numberArray = testNumber.MakeArray();
+      char[] testArray = {'5', '3', '2'};
+      CollectionAssert.AreEqual(numberArray, testArray);
+    }
+
+    [TestMethod]
+    public void FindMatch_ArrayReturnsString_String()
+    {
+      Numbers testNumber = new Numbers("532");
+      char[] numberArray = testNumber.MakeArray();
+      string findMatch = testNumber.FindMatch(numberArray);
+      Assert.AreEqual(findMatch, "five hundred thirty two");
+    }
   }
 }
--- Numbers/Models/Number.cs
+++ Numbers/Models/Number.cs
@@ -6,9 +6,24 @@ namespace NumbersToWords
   public class Numbers
   {
     private string _number;
-    private static Dictionary<int, string> _onesPlace = new Dictionary<int, string> ()
+    private static Dictionary<string, string> _onesPlace = new Dictionary<string, string> ()
     {
-      {1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}, {6, "six"}, {7, "seven"}, {8, "eight"}, {9, "nine"}
+      {"1", "one"}, {"2", "two"}, {"3", "three"}, {"4", "four"}, {"5", "five"}, {"6", "six"}, {"7", "seven"}, {"8", "eight"}, {"9", "nine"}
+    };
+
+    private static Dictionary<string, string> _tenToTwenty = new Dictionary<string, string> ()
+    {
+      {"10", "ten"}, {"11", "eleven"}, {"12", "twelve"}, {"13", "thirteen"}, {"14", "fourteen"}, {"15", "fifteen"}, {"16", "sixteen"}, {"17", "seventeen"}, {"18", "eighteen"}, {"19", "nineteen"}
+    };
+
+    private static Dictionary<string, string> _tensPlace = new Dictionary<string, string> ()
+    {
+      {"2", "twenty"}, {"3", "thirty"}, {"4", "forty"}, {"5", "fifty"}, {"6", "sixty"}, {"7", "seventy"}, {"8", "eighty"}, {"9", "ninety"}
+    };
+
+    private static Dictionary<string, string> _otherPlaces = new Dictionary<string, string> ()
+    {
+      {"3", "hundred"}, {"4", "thousand"}, {"6", "million"}, {"9", "billion"}, {"12", "trillion"}
     };
 
     public Numbers(string number)
@@ -25,5 +40,13 @@ namespace NumbersToWords
     {
       _number = newNumber;
     }
+
+    public char[] MakeArray()
+    {
+      char[] numberArray = _number.ToCharArray();
+      return numberArray;
+    }
+
+    
   }
 }
